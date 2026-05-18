2e4116d82cd0b556621e22e6695dd3222eac39d1
EXCALIBUR <reisyusuf.1903@gmail.com>
2026-05-18 19:23:20 +0300

    complete dockerfile and docker-compose configurations

--- api-gateway/api-gateway/src/main/java/api_gateway/api_gateway/Config/RouterValidator.java
+++ api-gateway/api-gateway/src/main/java/api_gateway/api_gateway/Config/RouterValidator.java
@@ -12,7 +12,7 @@ public class RouterValidator {
 
     public static final List<String> openApiEndpoints = List.of(
             "/v1/auth/login",
-            "/v1/auth/register",
+            "/v1/auth/internal/create",
             "/v1/auth/refresh-token",
             "/swagger-ui",
             "/swagger-ui.html",
